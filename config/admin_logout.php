462995e319275c86f4ed07625e2eda8f9873260d
ch!no <chino_garado@bethel.edu.ph>
2026-03-24 09:20:30 +0800

    Create admin_logout.php

---
+++ config/admin_logout.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'config/database.php';
+require_once 'config/auth.php';
+
+$database = new Database();
+$db = $database->getConnection();
+$auth = new Auth($db);
+
+$auth->logout();
+header('Location: index.php');
+exit();
+?>
